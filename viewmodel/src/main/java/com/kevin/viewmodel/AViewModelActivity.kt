c9d26bd2e5c5eee2c6b5a00a1511003d20c984f3
ZhangXin <zhangx@fxyb.com>
2021-09-30 17:20:30 +0800

    1.ViewModel创建实例方式

--- viewmodel/src/main/java/com/kevin/viewmodel/AViewModelActivity.kt
+++ viewmodel/src/main/java/com/kevin/viewmodel/AViewModelActivity.kt
@@ -17,11 +17,11 @@ class AViewModelActivity : AppCompatActivity(), View.OnClickListener {
         findViewById<Button>(R.id.btn_number).setOnClickListener(this)
         //方法一:官方提供的viewModel新建示例方式
         mAViewMode =
-            ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(
-                AViewMode::class.java
-            )
+                ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(
+                        AViewMode::class.java
+                )
         //方法二:通过构造函数示例
-        //推荐官方工厂模式:会通过HashMap去重，避免出现一个viewModel中的多个对象在生命周期.
+        //推荐官方工厂模式:会通过HashMap去重，避免出现一个viewModel中的多个对象在生命周期.以及会缓存viewModel对象
         //mAViewMode = AViewMode(application)
         mTvNumber.text = mAViewMode.number.toString()
     }
